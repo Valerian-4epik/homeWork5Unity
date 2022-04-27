1854d2ce5c6e18ec992d7061ccb276377d524e45
Valerian-4epik <98113328+Valerian-4epik@users.noreply.github.com>
2022-04-27 19:31:43 +0300

    v7

--- HomeWork#5/Assets/Sсripts/Alarm.cs
+++ HomeWork#5/Assets/Sсripts/Alarm.cs
@@ -6,27 +6,14 @@ public class Alarm : MonoBehaviour
 {
     [SerializeField] private AudioSource _audioSource;
 
-    private float _volume = 0.1f;
     private float _incrementStep = 0.2f;
     private bool _isInside = false;
     private float _maxVolume = 1.0f;
     private float _minVolume = 0.0f;
 
-    private void Start()
-    {
-        _audioSource.volume = _volume;
-    }
-
     private void Update()
     {
-        if(_isInside == true)
-        {
-            VolumeUp();
-        }
-        else
-        {
-            VolumeDown();
-        }
+        StartCoroutine(VolumeValue());
     }
 
     public void Play()
@@ -40,13 +27,29 @@ public class Alarm : MonoBehaviour
         _isInside = false;
     }
 
-    private void VolumeUp()
+    private IEnumerator VolumeValue()
     {
-        _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, _maxVolume, Time.deltaTime * _incrementStep);
+
+        if (_isInside == true)
+        {
+            ChangeVolume(_maxVolume);
+        }
+        else
+        {
+            ChangeVolume(_minVolume);
+
+            if(_audioSource.volume == _minVolume)
+            {
+                _audioSource.Stop();
+            }
+        }
+
+        yield return null;
     }
 
-    private void VolumeDown()
+    private void ChangeVolume(float value)
     {
-        _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, _minVolume, Time.deltaTime * _incrementStep);
+        _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, value, Time.deltaTime * _incrementStep);
     }
 }
+ 
